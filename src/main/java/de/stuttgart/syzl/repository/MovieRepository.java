ef1070bd2732726fa33d9a74c977fdd4669d0053
Georg <georgkromer@pm.me>
2020-12-14 19:00:34 +0100

    replaces mySQL with mariaDB

--- src/main/java/de/stuttgart/syzl/repository/MovieRepository.java
+++ src/main/java/de/stuttgart/syzl/repository/MovieRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
     Movie findById(Long id);
-    Movie findBySourceId(String SourceId);}
+    Movie findBySourceId(String SourceId);
+}
--- src/main/java/de/stuttgart/syzl/repository/SeriesRepository.java
+++ src/main/java/de/stuttgart/syzl/repository/SeriesRepository.java
@@ -3,6 +3,7 @@ package de.stuttgart.syzl.repository;
 import de.stuttgart.syzl.entity.Movie;
 import de.stuttgart.syzl.entity.Series;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 
 @Repository
